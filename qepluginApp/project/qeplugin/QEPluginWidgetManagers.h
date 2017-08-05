624b9b0d957d27d464cdef1d9b2e26f1944eb45e
Bob Gunion <rfgunion@lbl.gov>
2017-08-05 14:32:44 -0700

    Copied QT_VERSION preprocessor macros to qeplugin header files because otherwise they are ignored by moc

--- qepluginApp/project/qeplugin/QEPluginWidgetManagers.h
+++ qepluginApp/project/qeplugin/QEPluginWidgetManagers.h
@@ -26,6 +26,17 @@
 #ifndef QE_PLUGIN_WIDGET_MANAGERS_H
 #define QE_PLUGIN_WIDGET_MANAGERS_H
 
+// The QT_VERSION check is in QEDesignerPluginCommon.h, but does not
+// work with moc unless restated here.  Possibly relevant:
+// https://stackoverflow.com/questions/4119688/macro-expansion-in-moc
+// Provide Qt version independent plugin-related includes.
+//
+#if (QT_VERSION >= 0x050500)
+#include <QtUiPlugin/QDesignerCustomWidgetInterface>
+#else
+#include <QDesignerCustomWidgetInterface>
+#endif
+
 #include <QObject>
 #include <QEDesignerPluginCommon.h>
 #include <QEPluginLibrary_global.h>
