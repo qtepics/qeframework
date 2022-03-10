9e8a2e35d37a5b336fe8f0d47ac28ff3dbf6b8f4
Andrew Starritt <andrew.starritt@gmail.com>
2022-03-10 19:37:22 +1100

    Start 3.8.2 development

--- qeframeworkSup/project/common/QEFrameworkVersion.h
+++ qeframeworkSup/project/common/QEFrameworkVersion.h
@@ -3,7 +3,7 @@
  *  This file is part of the EPICS QT Framework, initially developed at the
  *  Australian Synchrotron.
  *
- *  Copyright (c) 2012-2021 Australian Synchrotron
+ *  Copyright (c) 2012-2022 Australian Synchrotron
  *
  *  The EPICS QT Framework is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -46,7 +46,7 @@
 //
 #define QE_VERSION_MAJOR      3     // Public releases that include interface or paradigm changes that may require changes to the way the framework is used.
 #define QE_VERSION_MINOR      8     // Public releases containing significant new functionality (may include fixes as well)
-#define QE_VERSION_RELEASE    1     // Public releases containing backward compatible new functionality and/or fixes.
+#define QE_VERSION_RELEASE    2     // Public releases containing backward compatible new functionality and/or fixes.
 
 // The integer version is (major << 16) + (minor << 8) + release, and this macro
 // is used to constuct an integer version number.
@@ -65,7 +65,7 @@
 //
 #define QE_VERSION_STAGE_DEVELOPMENT "Development"
 #define QE_VERSION_STAGE_PRODUCTION  "Production"
-#define QE_VERSION_STAGE             QE_VERSION_STAGE_PRODUCTION
+#define QE_VERSION_STAGE             QE_VERSION_STAGE_DEVELOPMENT
 
 #define QE_VERSION_STRING     STRINGIFY(QE_VERSION_MAJOR) "." \
                               STRINGIFY(QE_VERSION_MINOR) "." \
