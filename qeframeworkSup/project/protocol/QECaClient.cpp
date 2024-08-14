f52c188eea7645bcf0310bc2816791a388fae654
Andrew Starritt <andrew.starritt@gmail.com>
2024-08-14 16:22:46 +1000

    Add more control to the inclusion of PV Access, ie default YES or NO. Ensure  str.toStdString().c_str() operation stays in scope.

--- qeframeworkSup/project/protocol/QECaClient.cpp
+++ qeframeworkSup/project/protocol/QECaClient.cpp
@@ -3,7 +3,7 @@
  *  This file is part of the EPICS QT Framework, initially developed at the
  *  Australian Synchrotron.
  *
- *  Copyright (C) 2018-2023 Australian Synchrotron
+ *  Copyright (C) 2018-2024 Australian Synchrotron
  *
  *  The EPICS QT Framework is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -220,7 +220,8 @@ bool QECaClient::putPvData (const QVariant& value)
                // Treat as long string.
                QString str = value.toString ();
                const int len = str.length();
-               const char* text = str.toStdString().c_str();
+               const std::string keepInScope = str.toStdString();
+               const char* text = keepInScope.c_str();
                result = this->putByteArray (text, len + 1); // include the zero
             } else {
                // Treat as numeric.
