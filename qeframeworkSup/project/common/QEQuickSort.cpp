bc2d585995e2781df38f86eee6decbccaa0d17e5
Andrew Starritt <andrew.starritt@gmail.com>
2025-09-10 16:52:55 +1000

    QEQuickSort: use more meaningful variable names, improve comments, do special check for three items otherwise no change.

--- qeframeworkSup/project/common/QEQuickSort.cpp
+++ qeframeworkSup/project/common/QEQuickSort.cpp
@@ -3,6 +3,8 @@
  *  This file is part of the EPICS QT Framework, initially developed at the
  *  Australian Synchrotron.
  *
+ *  Copyright (c) 2016-2025 Australian Synchrotron.
+ *
  *  The EPICS QT Framework is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
@@ -16,16 +18,17 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with the EPICS QT Framework.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  Copyright (c) 2016,2017 Australian Synchrotron.
- *
  *  Author:
  *    Andrew Starritt
  *  Contact details:
- *    andrew.starritt@synchrotron.org.au
+ *    andrews@ansto.gov.au
  *
  */
 
 #include "QEQuickSort.h"
+#include <QDebug>
+
+#define DEBUG qDebug () << "QEQuickSort" << __LINE__ << __FUNCTION__ << " "
 
 //------------------------------------------------------------------------------
 //
@@ -41,47 +44,66 @@ QEQuickSort::~QEQuickSort() { }   // place holder
 //
 void QEQuickSort::sort (const int first, const int last, QObject* context)
 {
-   const int n = last - first + 1;
-   if (n <= 1) return; // nothing to do.
+   const int number = last - first + 1;
 
-   if (n == 2) {  // degenerate case
+   if (number <= 1) return; // nothing to do.
+
+   if (number == 2) {  // degenerate case
       if (this->itemLessThan (last, first, context)) {
          this->swapItems (first, last, context);
       }
-      return; // nothing more to do.
+      return;   // nothing more to do.
    }
 
-   // Three or more items.
-   // pivot the set of items about about last.
+   if (number == 3) {   // simple case - saves about 18%
+      const int middle = first + 1;
+      if (this->itemLessThan (middle, first, context)) {
+         this->swapItems (middle, first, context);
+      }
+      if (this->itemLessThan (last, middle, context)) {
+         this->swapItems (last, middle, context);
+      }
+      if (this->itemLessThan (middle, first, context)) {
+         this->swapItems (middle, first, context);
+      }
+      return;   // nothing more to do.
+   }
+
+   // Special for four items - prob not worth the effort.
+
+   // Four or more items.
+   // Partition about a pivot value (use last/right most element)
    //
-   int p = last;
-   int f = first;
-   int l = last - 1;
+   int pivot = last;
+   int left = first;
+   int right = last - 1;
 
    while (true) {
-      while ((f < p) && !this->itemLessThan (p, f, context)) {  // while f <= p
-         f++;
+      // while left <= pivot
+      while ((left < pivot) && !this->itemLessThan (pivot, left, context)) {
+         left++;
       }
-      if (f == p) break;
+      if (left == pivot) break;
 
-      // f > p
-      this->swapItems (f, p, context);
-      p = f; f++;
+      // left > pivot
+      this->swapItems (left, pivot, context);
+      pivot = left; left++;
 
-      while ((l > p) && !this->itemLessThan (l, p, context)) {  // while l >= p
-         l--;
+      // while right >= pivot
+      while ((right > pivot) && !this->itemLessThan (right, pivot, context)) {
+         right--;
       }
-      if (l == p) break;
+      if (right == pivot) break;
 
-      // l < p
-      this->swapItems (l, p, context);
-      p = l; l--;
+      // right < pivot
+      this->swapItems (right, pivot, context);
+      pivot = right; right--;
    }
 
    // Now qsort each sub-list.
    //
-   this->sort (first, p - 1, context);
-   this->sort (p + 1, last, context);
+   this->sort (first, pivot - 1, context);
+   this->sort (pivot + 1, last, context);
 }
 
 // end
--- qeframeworkSup/project/common/QEQuickSort.h
+++ qeframeworkSup/project/common/QEQuickSort.h
@@ -3,6 +3,8 @@
  *  This file is part of the EPICS QT Framework, initially developed at the
  *  Australian Synchrotron.
  *
+ *  Copyright (c) 2016-2025 Australian Synchrotron.
+ *
  *  The EPICS QT Framework is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
@@ -16,12 +18,10 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with the EPICS QT Framework.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  Copyright (c) 2016,2017 Australian Synchrotron.
- *
  *  Author:
  *    Andrew Starritt
  *  Contact details:
- *    andrew.starritt@synchrotron.org.au
+ *    andrews@ansto.gov.au
  *
  */
 
@@ -60,7 +60,7 @@ protected:
    //
    virtual bool itemLessThan (const int a, const int b, QObject* context) const = 0;
 
-   // The sub class must provide the means to swap two items.
+   // The sub class must provide the means to swap two items at index a and b.
    //
    // Examples
    // Standard array data:   temp = data [a]; data [a] = data [b]; data [b] = temp;
