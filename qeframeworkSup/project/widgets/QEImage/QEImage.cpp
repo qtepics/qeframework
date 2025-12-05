00dd7f9631d47212dd170c17f7f5a77d5532de94
Andrew Starritt <andrew.starritt@gmail.com>
2025-12-05 14:43:07 +1100

    QEImage: Missed converting imageDataFormats::formatOptions to QE::ImageFormatOptions in the mpeg signal in QEImage.

--- qeframeworkSup/project/widgets/QEImage/QEImage.cpp
+++ qeframeworkSup/project/widgets/QEImage/QEImage.cpp
@@ -3,12 +3,11 @@
  *  This file is part of the EPICS QT Framework, initially developed at the
  *  Australian Synchrotron.
  *
- *  Copyright (c) 2012-2023 Australian Synchrotron
+ *  Copyright (c) 2012-2025 Australian Synchrotron
  *
  *  The EPICS QT Framework is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ *  the Free Software Foundation, version 3 of the License.
  *
  *  The EPICS QT Framework is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,10 +17,9 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with the EPICS QT Framework.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  Author:
- *    Andrew Rhyder
- *  Contact details:
- *    andrew.rhyder@synchrotron.org.au
+ *  Author:     Andrew Rhyder
+ *  Maintainer: Andrew Starritt
+ *  Contact:    andrews@ansto.gov.au
  */
 
 /*
@@ -191,14 +189,17 @@ void QEImage::setup()
     }
 
     // Connect MPEG data source.
+    //
     QObject::connect (mpegSource, SIGNAL (setDataImage( const QByteArray&,
                                                         unsigned long, unsigned long,
                                                         unsigned long, unsigned long,
-                                                        imageDataFormats::formatOptions, unsigned int )),
+                                                        QE::ImageFormatOptions,
+                                                        unsigned int )),
                       this,       SLOT   (setDataImage( const QByteArray&,
                                                         unsigned long, unsigned long,
                                                         unsigned long, unsigned long,
-                                                        imageDataFormats::formatOptions, unsigned int )));
+                                                        QE::ImageFormatOptions,
+                                                        unsigned int )));
 
     // Create the video destination
     videoWidget = new VideoWidget( this );
