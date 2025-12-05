5d3901903b16ffeba25ef049a61a1153e43e37a8
Andrew Starritt <andrew.starritt@gmail.com>
2025-12-05 14:47:07 +1100

    UserMessage: ensure MESSAGE_TYPE_INFO starts from 0, tweak comments.

--- qeframeworkSup/project/widgets/QEWidget/UserMessage.h
+++ qeframeworkSup/project/widgets/QEWidget/UserMessage.h
@@ -3,12 +3,11 @@
  *  This file is part of the EPICS QT Framework, initially developed at the
  *  Australian Synchrotron.
  *
- *  Copyright (c) 2012-2022 Australian Synchrotron
+ *  Copyright (c) 2012-2025 Australian Synchrotron
  *
  *  The EPICS QT Framework is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ *  the Free Software Foundation, version 3 of the License.
  *
  *  The EPICS QT Framework is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,10 +17,9 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with the EPICS QT Framework.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  Author:
- *    Andrew Rhyder
- *  Contact details:
- *    andrew.rhyder@synchrotron.org.au
+ *  Author:     Andrew Rhyder
+ *  Maintainer: Andrew Starritt
+ *  Contact:    andrews@ansto.gov.au
  */
 
 #ifndef QE_USER_MESSAGE_H
@@ -36,7 +34,7 @@
 // Note: message_types is now a class (see below) - what was message_types is now message_severities.
 //
 enum message_severities {
-   MESSAGE_TYPE_INFO,
+   MESSAGE_TYPE_INFO = 0,
    MESSAGE_TYPE_WARNING,
    MESSAGE_TYPE_ERROR
 };
